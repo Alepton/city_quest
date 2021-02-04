64f0b61a19fc466f83cb82349164048bbcc59fe8
Andrei Kardovich <akardovich@gmail.com>
2021-02-04 20:28:13 +0300

    21 Page Dialog

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import c from './Dialogs.module.css';
+import React from "react";
+import c from "./Dialogs.module.css";
 
-const Dialogs = (props) =>{
+const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
+    <div className={c.dialogs}>
+      <div className={c.dialogItem}>
+        <div className={c.dialog}>Andrey</div>
+        <div className={c.dialog}>Natasha</div>
+        <div className={c.dialog}>Egor</div>
+        <div className={c.dialog}>Dasha</div>
+        <div className={c.dialog}>Nina</div>
+        <div className={c.dialog}>Igor</div>
+      </div>
+      <div className={c.messages}>
+        <div className={c.message}>Hi</div>
+        <div className={c.message}>How is your name?</div>
+        <div className={c.message}>My name is Pavel</div>
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Dialogs
+export default Dialogs;
