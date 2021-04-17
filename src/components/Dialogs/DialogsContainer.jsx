72efb429008271047595630d8426eef413b78f0e
Andrei Kardovich <akardovich@gmail.com>
2021-04-17 08:04:56 +0300

    48_copy_DialogReduser

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,36 +1,31 @@
 import React from "react";
 import c from "./Dialogs.module.css";
-import DialogItem from "./DialogItem/DialogsItem";
-import Messages from "./Message/Message";
 import {
   sendMessageCreator,
   updateNewMessageBodyCreator,
   updateNewPostTextActionCreator,
 } from "../../redux/dialogs-reducer";
 import Dialogs from "./Dialogs";
-import StoreContext from "../../StorContext";
+import { connect } from "react-redux";
 
-const DialogsContainer = () => {
-  return <StoreContext.Consumer>
-      { (store) => {
 
-        let state = store.getState();
+let mapStateToProps = (state) => {
+  return {
+    dialogsPage: state.dialogsPage
+  }
+}
 
-        let onSendMessageClick = () => {
-          store.dispatch(sendMessageCreator());
-        };
+let mapDispatchToProps = (dispatch) => {
+  return {
+    updateNewMessageBody: () => {
+      dispatch(sendMessageCreator());
+    },
+    sendMessage: (body) => {
+      dispatch(updateNewMessageBodyCreator(body));
+    }
+  }
+}
 
-        let onNewMessageChange = (body) => {
-          store.dispatch(updateNewMessageBodyCreator(body));
-        };
-
-        return <Dialogs
-            updateNewMessageBody={onNewMessageChange}
-            sendMessage={onSendMessageClick}
-            dialogsPage={state}/>
-      }
-      };
-    </StoreContext.Consumer>
-};
+const DialogsContainer = connect(mapStateToProps, mapDispatchToProps) (Dialogs);
 
 export default DialogsContainer;
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,5 +1,5 @@
-const ADD_POST = 'ADD-POST';
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
 let initialState = {
   posts: [
@@ -9,34 +9,40 @@ let initialState = {
     { id: 4, message: "It's my first post", likesCount: 20 },
     { id: 5, message: "It's my first post", likesCount: 20 },
     { id: 6, message: "It's my first post", likesCount: 20 },
-  ]
-}
+  ],
+};
 
 const profileReducer = (state = initialState, action) => {
-
-  switch(action.type) {
-    case ADD_POST:
+  switch (action.type) {
+    case ADD_POST: {
       let newPost = {
         id: 7,
         message: state.newPostText,
         likesCount: 0,
       };
 
-      state.posts.push(newPost);
-      state.newPostText = "";
-      return state;
+      return {
+        ...state,
+        posts: [...state.posts, newPost],
+        newPostText: "",
+      };
+    }
 
-    case UPDATE_NEW_POST_TEXT:
-      state.newPostText = action.newText;
-      return state;
-
+    case UPDATE_NEW_POST_TEXT: {
+      return {
+        ...state,
+        newPostText: action.newText,
+      };
+    }
     default:
       return state;
   }
-}
+};
 
 export const addPostActionCreator = () => ({ type: ADD_POST });
-export const updateNewPostTextActionCreator = (text) =>
-({ type: UPDATE_NEW_POST_TEXT, newText: text });
+export const updateNewPostTextActionCreator = (text) => ({
+  type: UPDATE_NEW_POST_TEXT,
+  newText: text,
+});
 
 export default profileReducer;
