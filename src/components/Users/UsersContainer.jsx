fca8c4a0709a7e01338ae0625cf48236f2969d5c
Andrei Kardovich <akardovich@gmail.com>
2021-04-25 21:36:19 +0300

    55_pagination

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { followActionCreator, setUsersActionCreator, unfollowActionCreator } from '../../redux/users-reducer';
+import { followActionCreator, setUsersActionCreator, unfollowActionCreator, setCurrentPageCreator, setTotalUsersCountCreator } from '../../redux/users-reducer';
 import Users from './Users';
 import c from './Users.module.css';
 
 // функция принимает state всего приложения и возвращает те данные которые нам нужны именно тут (список пользователей)
 let mapStateToProps = (state) => {
   return {
-    users: state.userPage.users
+    users: state.userPage.users,
+    pageSize: state.userPage.pageSize,
+    totalUsersCount: state.userPage.totalUsersCount,
+    currentPage: state.userPage.currentPage
   }
 }
 
@@ -26,6 +29,13 @@ let mapDispatchToProps = (dispatch) => {
       dispatch (setUsersActionCreator (users) );
     },
 
+    setCurrentPage: (pageNumber) => {
+      dispatch (setCurrentPageCreator (pageNumber))
+    },
+
+    setTotalUsersCount: (totalCount) => {
+      dispatch (setTotalUsersCountCreator (totalCount))
+    }
   }
 }
 
