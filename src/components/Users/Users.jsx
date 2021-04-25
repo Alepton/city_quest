0791f69fe454ad6d5a79bc4291b2cd4e0e94432f
Andrei Kardovich <akardovich@gmail.com>
2021-04-25 07:23:10 +0300

    52_side effekt

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,45 +1,28 @@
+import axios from 'axios';
 import React from 'react';
 import c from './Users.module.css';
+import userPhoto from '../../asserts/images/ava4.png';
 
 const Users = (props) =>{
+  let getUsers = () => {
 
   if (props.users.length === 0) {
-  props.setUsers ( [
-      { id: 1,
-        photoUrl:
-          "https://vokrug.tv/pic/news/a/d/a/4/ada46649ae337d1f9ddeb1a13b6803c7.jpg",
-        followed: true,
-        fullNeme: "Dmitry",
-        status: "I am a boss",
-        location: { city: "Minsk", country: "Belarus" }
-      },
-      { id: 2,
-        photoUrl:
-          "https://vokrug.tv/pic/news/a/d/a/4/ada46649ae337d1f9ddeb1a13b6803c7.jpg",
-        followed: false,
-        fullNeme: "Sasha",
-        status: "I am a boss too",
-        location: { city: "Moscow", country: "Rassia" }
-      },
-      { id: 3,
-        photoUrl:
-          "https://vokrug.tv/pic/news/a/d/a/4/ada46649ae337d1f9ddeb1a13b6803c7.jpg",
-        followed: true,
-        fullNeme: "Andrei",
-        status: "I am a big boss",
-        location: { city: "Dubai", country: "Amirats" }
-      }
-    ]
-    )
+    axios.get("https://social-network.samuraijs.com/api/1.0/users")
+    .then(response => {
+      props.setUsers (response.data.items);
+    });
+  }
+
   }
 
   return (
     <div>
+      <button onClick={getUsers}>Get Users</button>
       {
         props.users.map (u => <div key={u.id}>
           <span>
             <div>
-              <img src={u.photoUrl} className={c.userPhoto} />
+              <img src={u.photos.small != null ? u.photos.small : "userPhoto" } className={c.userPhoto} />
             </div>
             <div>
               {u.followed
@@ -49,12 +32,12 @@ const Users = (props) =>{
             </div>
           </span>
           <span>
-            <div>{u.fullName}</div>
+            <div>{u.name}</div>
             <div>{u.status}</div>
           </span>
           <span>
-            <div>{u.location.country}</div>
-            <div>{u.location.city}</div>
+            <div>{"u.location.country"}</div>
+            <div>{"u.location.city"}</div>
           </span>
         </div> )
       }
