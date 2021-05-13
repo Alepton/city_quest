b54bb2f41219d3ecd4bbbf47ddf5235680a989f9
Andrei Kardovich <akardovich@gmail.com>
2021-05-13 21:19:53 +0300

    64_button disabled

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: "https://social-network.samuraijs.com/api/1.0/",
+  headers: {
+    "API-KEY": "e3e6407a-9d65-4698-9429-3f97aaa4f1ee",
+  },
+});
+
+export const usersAPI = {
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance
+      .get(`users?page=${currentPage}&count=${pageSize}`)
+      .then((respons) => {
+        return respons.data;
+      });
+  }
+}
