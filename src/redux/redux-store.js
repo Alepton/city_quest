9da1c2a6e0004aefa028bbb407ee52b7e05ce732
Andrei Kardovich <akardovich@gmail.com>
2021-11-21 09:38:50 +0300

    '93_paginator_pageNamber'

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,23 +1,29 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import authReducer from "./auth-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import profileReducer from "./profile-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
 import thunkMiddleware from "redux-thunk";
-import { reducer as formReducer } from 'redux-form'
+import { reducer as formReducer } from "redux-form";
 import appReducer from "./app-reducer";
 
-let reducers = combineReducers ({
-  profilePage: profileReducer ,
+let reducers = combineReducers({
+  profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   sidebar: sidebarReducer,
   userPage: usersReducer,
   auth: authReducer,
   form: formReducer,
-  app: appReducer
+  app: appReducer,
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+);
+
+window.__store__ = store;
 
 export default store;
