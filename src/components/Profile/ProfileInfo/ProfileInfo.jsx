b01e7b435872758c8945c3b224d0d8a0a9957676
Andrei Kardovich <akardovich@gmail.com>
2021-11-22 06:57:42 +0300

    '95_GitHub_pages'

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,24 +2,23 @@ import React from "react";
 import Preloader from "../../common/Preloader/Preloader";
 import c from "./ProfileInfo.module.css";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
+import userPhoto from "../../../asserts/images/ava1.png";
 
-const ProfileInfo = (profile, status, updateStatus) => {
+const ProfileInfo = ({profile, status, updateStatus}) => {
   if (!profile) {
     return <Preloader />;
   }
 
   return (
     <div>
-      <div className="profileInfo">
         <div>
           <img src="https://snova-prazdnik.ru/wp-content/uploads/2015/03/kvest_real_01-1440x564_c.jpg"></img>
         </div>
 
         <div className={c.discriptionBlock}>
-          <img src={profile.photos.large} />
+          <img src={profile.photos.large || userPhoto} />
           <ProfileStatusWithHooks status={status} updateStatus={updateStatus} />
         </div>
-      </div>
     </div>
   );
 };
